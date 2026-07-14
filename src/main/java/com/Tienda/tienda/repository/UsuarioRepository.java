5105d9095718597c6a381bd181f1cd9a25ffc8d2
Neytan <neytanbourrouet@gmail.com>
2026-07-13 19:07:28 -0600

    Clase10

---
+++ src/main/java/com/Tienda/tienda/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.Tienda.tienda.repository;
+
+import com.Tienda.tienda.domain.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    
+    public Optional<Usuario> findByUsernameAndActivoTrue(String username);
+    
+}
---
+++ src/main/java/com/Tienda/tienda/service/RutaService.java
@@ -0,0 +1,23 @@
+package com.Tienda.tienda.service;
+
+import com.Tienda.tienda.domain.Ruta;
+import com.Tienda.tienda.repository.RutaRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RutaService {
+    
+    private final RutaRepository rutaRepository;
+
+    public RutaService(RutaRepository rutaRepository) {
+        this.rutaRepository = rutaRepository;
+    }
+    
+    @Transactional(readOnly=true)
+    public List<Ruta> getRutas() {
+        return rutaRepository.findAllByOrderByRequiereRolAsc();
+    }
+    
+}
